5c2be1305dc8aa12261771ab04a4fec2d7467c48
Sergey Podolsky <sergey.podolsky@gmail.com>
2015-05-16 19:41:25 +0300

    import projects from svn

---
+++ SaxonTest/ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    using System.IO;
+    using System.Xml;
+
+    using Saxon.Api;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xml = "<root attr1='42' attr2=''/>";
+            var xpath = "//root/name()";
+            var evaluator = new XPathEvaluator();
+            var document = evaluator.CreateDocument(xml);
+            var result = evaluator.Evaluate(document, xpath);
+            Console.WriteLine(result.GetType());
+        }
+
+        class XPathEvaluator
+        {
+            private readonly Processor processor;
+            private readonly XPathCompiler xpathCompiler;
+            private DocumentBuilder documentBuilder;
+
+            public XPathEvaluator()
+            {
+                processor = new Processor();
+                xpathCompiler = processor.NewXPathCompiler();
+                documentBuilder = processor.NewDocumentBuilder();
+                documentBuilder.BaseUri = new Uri("c://dummy");
+            }
+
+            public XdmValue Evaluate(XdmItem xml, string xpath)
+            {
+                XPathExecutable xpathExecutable = xpathCompiler.Compile(xpath);
+                XPathSelector xpathSelector = xpathExecutable.Load();
+                xpathSelector.ContextItem = xml;
+                return xpathSelector.Evaluate();
+            }
+
+            public XdmNode CreateDocument(string xml)
+            {
+                var stringReader = new StringReader(xml);
+                return documentBuilder.Build(stringReader);
+            }
+        }
+    }
+}
